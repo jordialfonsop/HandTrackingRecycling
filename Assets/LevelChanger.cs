bdaaadb8ed5f3ab924d4c8fc6c38065bff327a92
jordialfonsop <jordialfonsop@gmail.com>
2023-02-22 11:09:12 +0100

    Load level Game at Main Menu

--- Assets/LevelChanger.cs
+++ Assets/LevelChanger.cs
@@ -1,13 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement; 
 
 public class LevelChanger : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        Application.Quit();
+        SceneManager.LoadScene("Game"); 
     }
 
     // Update is called once per frame
