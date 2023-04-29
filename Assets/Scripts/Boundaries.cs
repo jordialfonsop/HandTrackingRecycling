24871f6e698070e47b633a80d410d7ccaf6bd96f
jordialfonsop <jordialfonsop@gmail.com>
2023-04-29 11:57:50 +0200

    Set up boundaries scene and script

---
+++ Assets/Scripts/Boundaries.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.XR.OpenXR;
+using UnityEngine;
+using UnityEngine.XR.OpenXR.Input;
+
+public class Boundaries : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        //OpenXRDeviceBoundaries boundaries = OpenXRDevice.boundaries;
+        //Debug.Log("Device boundaries: " + boundaries);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
