95fce1cd976a361b9310e7620e68e2b35b79a5a2
jordialfonsop <jordialfonsop@gmail.com>
2023-05-01 13:25:14 +0200

    Implemented boundaries script + prefab

--- Assets/Scripts/Boundaries.cs
+++ Assets/Scripts/Boundaries.cs
@@ -3,21 +3,43 @@ using System.Collections.Generic;
 using UnityEngine.XR.OpenXR;
 using UnityEngine;
 using UnityEngine.XR.OpenXR.Input;
+using UnityEngine.XR.Management;
+using UnityEngine.XR;
 
 public class Boundaries : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private List<Vector3> boundaries = new List<Vector3>();
+
+    public List<Vector3> GetBoundaries()
     {
-
-        //OpenXRDeviceBoundaries boundaries = OpenXRDevice.boundaries;
-        //Debug.Log("Device boundaries: " + boundaries);
-
+        return boundaries;
     }
 
-    // Update is called once per frame
-    void Update()
+    void Start()
     {
-        
+        var loader = XRGeneralSettings.Instance?.Manager?.activeLoader;
+        if (loader == null)
+        {
+            Debug.LogWarning("Could not get active Loader.");
+            return;
+        }
+        var inputSubsystem = loader.GetLoadedSubsystem<XRInputSubsystem>();
+        inputSubsystem.boundaryChanged += InputSubsystem_boundaryChanged;
+    }
+    private void InputSubsystem_boundaryChanged(XRInputSubsystem inputSubsystem)
+    {
+        List<Vector3> boundaryPoints = new List<Vector3>();
+        if (inputSubsystem.TryGetBoundaryPoints(boundaryPoints))
+        {
+            foreach (var point in boundaryPoints)
+            {
+                Debug.Log(point);
+            }
+            boundaries = boundaryPoints;
+        }
+        else
+        {
+            Debug.LogWarning($"Could not get Boundary Points for Loader");
+        }
     }
 }
